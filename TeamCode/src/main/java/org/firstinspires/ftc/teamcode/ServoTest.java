9ecaa60ba371d81ee0a5bd51075968a8478106a8
KGolakoti <kk6789@gmail.com>
2018-08-19 15:06:29 -0600

    no message

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "TestServo", group = "Sample")
+public class ServoTest extends LinearOpMode
+{
+
+
+    //Declare Servos
+    private Servo RightArmServo;
+    private Servo LeftArmServo;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+
+
+        LeftArmServo = hardwareMap.servo.get("LeftArmServo");
+        RightArmServo = hardwareMap.servo.get("RightArmServo");
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+
+
+
+
+
+
+            if (gamepad1.b) {
+                LeftArmServo.setPosition(.52);
+                RightArmServo.setPosition(.39);
+            }else{
+                     if (gamepad1.a) {
+                         LeftArmServo.setPosition(.8);
+                         RightArmServo.setPosition(.17);
+                     }
+                 }
+
+
+            idle();
+        }
+    }
+}
