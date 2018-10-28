66975a7d2795af3797400cf0801b561951be5f6d
KGolakoti <kk6789@gmail.com>
2018-10-28 16:56:13 -0600

    full teleop for october 28

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp1.java
@@ -10,21 +10,22 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class TeleOp1 extends LinearOpMode
 {
     //declare motors
-    //private DcMotor latchLeftM;
-    //private DcMotor latchRightM;
+    private DcMotor latchLeftM;
+    private DcMotor latchRightM;
     private DcMotor driveFLM;
     private DcMotor driveFRM;
     private DcMotor driveBLM;
     private DcMotor driveBRM;
-    //private DcMotor armUpDownM;
-    //private DcMotor collectorM;
+    private DcMotor armUpDownM;
+    //private DcMotor armExtenderM;
+
 
     //declare servos
-    //private Servo latchLeftS;
-    //private Servo latchRightS;
+    private Servo latchLeftS;
+    private Servo latchRightS;
+    private Servo collectorS;
     //private Servo samplingS;
-    //private Servo collectorUpDOwnS;
-    //private Servo armExtenderS;
+    private Servo collectorUpDOwnS;
 
 
     @Override
@@ -32,30 +33,31 @@ public class TeleOp1 extends LinearOpMode
     {
 
         //declare motors
-        //latchLeftM = hardwareMap.dcMotor.get("latchLeftM");
-        //latchRightM = hardwareMap.dcMotor.get("latchRightM");
+        latchLeftM = hardwareMap.dcMotor.get("latchLeftM");
+        latchRightM = hardwareMap.dcMotor.get("latchRightM");
         driveFLM = hardwareMap.dcMotor.get("driveFLM");
         driveFRM = hardwareMap.dcMotor.get("driveFRM");
         driveBLM = hardwareMap.dcMotor.get("driveBLM");
         driveBRM = hardwareMap.dcMotor.get("driveBRM");
-        //armUpDownM = hardwareMap.dcMotor.get("armUpDownM");
-        //collectorM = hardwareMap.dcMotor.get("collectorM");
+        armUpDownM = hardwareMap.dcMotor.get("armUpDownM");
+        //armExtenderM = hardwareMap.dcMotor.get("armExtenderM");
+
 
 
         //reversing necessary motors
-        //latchLeftM.setDirection(DcMotor.Direction.REVERSE);
+        latchLeftM.setDirection(DcMotor.Direction.REVERSE);
         driveFLM.setDirection(DcMotor.Direction.REVERSE);
         driveBLM.setDirection(DcMotor.Direction.REVERSE);
 
         //declare servos
-        //latchLeftS = hardwareMap.servo.get("latchLeftS");
-        //latchRightS = hardwareMap.servo.get("latchRightS");
+        latchLeftS = hardwareMap.servo.get("latchLeftS");
+        latchRightS = hardwareMap.servo.get("latchRightS");
+        collectorS = hardwareMap.servo.get("collectorS");
         //samplingS = hardwareMap.servo.get("samplingS");
-        //collectorUpDOwnS = hardwareMap.servo.get("collectorUpDOwnS");
-        //armExtenderS = hardwareMap.servo.get("armExtenderS");
+        collectorUpDOwnS = hardwareMap.servo.get("collectorUpDownS");
 
-        //latchLeftS.setPosition(0);
-        //latchRightS.setPosition(1);
+        latchLeftS.setPosition(0);
+        latchRightS.setPosition(1);
 
         //declare drivespeed variable. this is to change the speed that the robot moves
         double drivespeed;
@@ -91,38 +93,58 @@ public class TeleOp1 extends LinearOpMode
             }
 
 
-
+            //drive the robot
             driveFLM.setPower(-gamepad1.left_stick_y/drivespeed);
             driveFRM.setPower(-gamepad1.right_stick_y/drivespeed);
             driveBLM.setPower(-gamepad1.left_stick_y/drivespeed);
             driveBRM.setPower(-gamepad1.right_stick_y/drivespeed);
 
-            //latchLeftM.setPower(-gamepad2.left_stick_y/latchspeed);
-            //latchRightM.setPower(-gamepad2.left_stick_y/latchspeed);
+            if(gamepad1.right_bumper)
+            {
+                armUpDownM.setPower(0.5);
+            }else{
+                    if(gamepad1.left_bumper)
+                    {
+                        armUpDownM.setPower(-0.5);
+                    }
+                 }
 
-            /*if (gamepad2.b) {
+
+            //move the latch thing
+            latchLeftM.setPower(-gamepad2.left_stick_y/latchspeed);
+            latchRightM.setPower(-gamepad2.left_stick_y/latchspeed);
+
+
+            if (gamepad2.left_trigger > .1)
+            {
                 latchLeftS.setPosition(0);
                 latchRightS.setPosition(1);
             }else{
-                if (gamepad2.a) {
-                    latchLeftS.setPosition(1);
-                    latchRightS.setPosition(0);
-                }
-            }*/
+                    if (gamepad2.right_trigger > .1)
+                    {
+                        latchLeftS.setPosition(1);
+                        latchRightS.setPosition(0);
+                    }
+                 }
 
-            /*if(gamepad2.dpad_down){
-                collectorM.setPower(-1);
+
+            if(gamepad2.a)
+            {
+                collectorS.setDirection(Servo.Direction.REVERSE);
             }else{
-                if(gamepad2.dpad_up){
-                    collectorM.setPower(1);
-                }else{
-                    if(gamepad2.dpad_left){
-                        collectorM.setPower(0);
+                  if(gamepad2.y)
+                  {
+                    collectorS.setDirection(Servo.Direction.FORWARD);
+                  }else{
+                        if(gamepad2.b)
+                        {
+                            collectorS.setPosition(.50);
+                        }
                     }
                 }
-            }*/
 
 
+            collectorUpDOwnS.setPosition(gamepad2.right_stick_y/2 +0.5);
 
 
             idle();
