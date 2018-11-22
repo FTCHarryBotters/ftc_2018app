b79693d795532a407422e252de5c24df2c8f07c7
LAPTOP-HCK25EM8\kvg67 <thusharreddy@gmail.com>
2018-11-21 17:32:25 -0700

    02018-11-21-2 >created a gyroscope test program. it can telemetrize the angle of the robot. >The roll and pitch may be reversed, but they are not as important as the yaw. >after that, i made a simple code that turns right 45 degrees Fahrenheit >i will now make a method that turns on gyro in the Autonomous depot and crater.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroscopeTest.java
@@ -0,0 +1,152 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+import java.util.Locale;
+
+@Autonomous(name = "GyroscopeTest", group = "Sample")
+public class GyroscopeTest extends LinearOpMode{
+    //declaration
+    private DcMotor driveFLM;
+    private DcMotor driveFRM;
+    private DcMotor driveBLM;
+    private DcMotor driveBRM;
+
+    private BNO055IMU GyroS;
+    Orientation angles;
+    Acceleration gravity;
+
+    public void runOpMode() throws InterruptedException {
+        //configuration
+        driveFLM = hardwareMap.dcMotor.get("driveFLM");
+        driveFRM = hardwareMap.dcMotor.get("driveFRM");
+        driveBLM = hardwareMap.dcMotor.get("driveBLM");
+        driveBRM = hardwareMap.dcMotor.get("driveBRM");
+        driveFLM.setDirection(DcMotor.Direction.FORWARD);
+        driveFRM.setDirection(DcMotor.Direction.REVERSE);
+        driveBLM.setDirection(DcMotor.Direction.FORWARD);
+        driveBRM.setDirection(DcMotor.Direction.REVERSE);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        GyroS = hardwareMap.get(BNO055IMU.class, "GyroS");
+        GyroS.initialize(parameters);
+
+        composeTelemetry();
+
+        waitForStart();
+
+            GyroS.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
+        while (opModeIsActive()) {
+
+            telemetry.update();
+
+            if(angles.firstAngle > -45)
+            {
+                spin(0.2);
+            }else{
+                stopmotors();
+            }
+
+        }
+    }
+    //methods
+
+    private void spin(double power) {
+        driveFLM.setPower(power);
+        driveFRM.setPower(-power);
+        driveBLM.setPower(power);
+        driveBRM.setPower(-power);
+    }
+    private void stopmotors(){
+        driveFLM.setPower(0);
+        driveFRM.setPower(0);
+        driveBLM.setPower(0);
+        driveBRM.setPower(0);
+    }
+    private void composeTelemetry() {
+
+        // At the beginning of each telemetry update, grab a bunch of data
+        // from the IMU that we will then display in separate lines.
+        telemetry.addAction(new Runnable() { @Override public void run()
+        {
+            // Acquiring the angles is relatively expensive; we don't want
+            // to do that in each of the three items that need that info, as that's
+            // three times the necessary expense.
+            angles   = GyroS.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            gravity  = GyroS.getGravity();
+        }
+        });
+
+        telemetry.addLine()
+                .addData("status", new Func<String>() {
+                    @Override public String value() {
+                        return GyroS.getSystemStatus().toShortString();
+                    }
+                })
+                .addData("calib", new Func<String>() {
+                    @Override public String value() {
+                        return GyroS.getCalibrationStatus().toString();
+                    }
+                });
+
+        telemetry.addLine()
+                .addData("YAW", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.firstAngle);
+                    }
+                })
+                .addData("roll", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.secondAngle);
+                    }
+                })
+                .addData("pitch", new Func<String>() {
+                    @Override public String value() {
+                        return formatAngle(angles.angleUnit, angles.thirdAngle);
+                    }
+                });
+
+        telemetry.addLine()
+                .addData("grvty", new Func<String>() {
+                    @Override public String value() {
+                        return gravity.toString();
+                    }
+                })
+                .addData("mag", new Func<String>() {
+                    @Override public String value() {
+                        return String.format(Locale.getDefault(), "%.3f",
+                                Math.sqrt(gravity.xAccel*gravity.xAccel
+                                        + gravity.yAccel*gravity.yAccel
+                                        + gravity.zAccel*gravity.zAccel));
+                    }
+                });
+    }
+    String formatAngle(AngleUnit angleUnit, double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+    String formatDegrees(double degrees){
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
+}
